f72129eec6430ce9ba2b1e958ce21e9fb8423ad0
JJ's_fun_stuff <worameth.jojo@gmail.com>
2019-11-24 23:44:20 +0700

    Refactor

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,43 @@ package main
 import "testing"
 
 func TestHello(t *testing.T){
-	got := Hello("Chris")
-	want := "Hello Jojo"
+	
+	//**********************first version********************
+	// got := Hello("Jojo")
+	// want := "Hello Jojo"
 
-	if got != want {
-		t.Errorf("want %s got %s", want, got)
+	// if got != want {
+	// 	t.Errorf("want %s got %s", want, got)
+	// }
+
+	assertCorrectMessage := func(t *testing.T, got, want string){
+		t.Helper()
+		if got != want {
+			t.Errorf("want %s got %s", want, got)
+		}
 	}
+	t.Run("Saying Hello to people", func(t *testing.T){
+		got := Hello("Jojo","")
+		want := "Hello Jojo"
+		assertCorrectMessage(t, got, want)
+
+		
+	})
+
+	t.Run("Saying 'Hello World' when there is no argument in Hello()", func(t *testing.T){
+		got := Hello("","")
+		want := "Hello World"
+
+		assertCorrectMessage(t, got, want)
+
+
+	})
+
+	t.Run("In Spanish", func(t *testing.T){
+		got := Hello("Jose", "French")
+		want := "Bonjour Jose"
+
+		assertCorrectMessage(t,got,want)
+	})
+
 }
--- main.go
+++ main.go
@@ -2,13 +2,41 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
 
-func Hello(name string) string{
-	return "Hello "+ name
+const englishHelloPrefix = "Hello "
+const spanishHelloPrefix = "Hola "
+const frenchHelloPrefix = "Bonjour "
+
+func Hello(name string, language string) string {
+
+	if name == "" {
+		name = "World"
+	}
+	return greetingPrefix(language) + name
+}
+	// if language == spanish {
+	// 	return spanishHelloPrefix + name
+	// }
+
+	// if language == french {
+	// 	return frenchHelloPrefix + name
+	// }
+func greetingPrefix(language string) (prefix string){
+
+	switch language{
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish: 
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return 
 }
 
+func main() {
 
-func main(){
-	
-	fmt.Println(Hello("Chris"))
+	fmt.Println(Hello("Jose", "French"))
 }
